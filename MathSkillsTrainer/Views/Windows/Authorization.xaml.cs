e375985343eecc2df82618b32514da2bf5e3be04
Майская М.А. <mayskaya_m.a@mail.ru>
2025-12-15 11:26:34 +0300

    стартовая архитектура

---
+++ MathSkillsTrainer/Views/Windows/Authorization.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MathSkillsTrainer.Helpers;
+using MathSkillsTrainer.ViewModels;
+using MathSkillsTrainer.Services.Interfaces;
+
+namespace MathSkillsTrainer.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml, подключение AuthorizationViewModel.cs
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        public Authorization(AuthorizationViewModel viewModel)
+        {
+            InitializeComponent();
+
+            DataContext = viewModel;
+        }
+
+    }
+}
